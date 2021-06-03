c96ef3dc17eeb10d61954f903195789ed659df22
gokul59 <gokul59g@gmail.com>
2021-06-03 13:08:30 +0000

    6-roll

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 
@@ -17,31 +15,16 @@ object Build : BuildType({
     vcs {
         root(DslContext.settingsRoot)
     }
-    features{
-        freeDiskSpace {
-            requiredSpace = "6gb"
-            failBuild = true
-        }
 
-    }
     steps {
         maven {
-            name = "Marco step"
-            goals = "clean test"
+            goals = "clean package"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = bundled_3_6()
-            jdkHome = "%env.JDK_16_0%"
         }
     }
 
     triggers {
         vcs {
-            quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_CUSTOM
-            quietPeriod = 30
-            perCheckinTriggering = true
-            groupCheckinsByCommitter = true
-            enableQueueOptimization = false
         }
-
     }
 })
