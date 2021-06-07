1ce4be0427a371756a9c1b75b6ddc3b663d479d4
gokul59 <gokul59g@gmail.com>
2021-06-07 09:52:41 +0000

    11-test

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,13 +1,23 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.project as project
+
 
 
 version = "2020.2"
 
 project {
+
     buildType(Build)
+
+
+    sequential {
+        buildType(Build)
+
+    }
 }
+
 
 object Build : BuildType({
     name = "Build"
@@ -18,13 +28,31 @@ object Build : BuildType({
 
     steps {
         maven {
-            goals = "clean package"
+            goals = "clean compile"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
+})
+object Package : BuildType({
+    name = "package"
 
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean compile"
+            runnerArgs = "-Dmaven.test.failure.ignore=true -DskipTest"
+        }
+    }
+
+    dependencies {
+        snapshot(Build) {}
+    }
     triggers {
         vcs {
         }
     }
 })
+
